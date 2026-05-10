9c4af4706b22ffdf3cb47b28e50c1c5f4ba6ba06
slingshot <Slingards@proton.me>
2026-05-10 10:57:49 +0100

    changed somethings

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-def create_app():
-    app = Flask(__name__)
-
-    from app.routes.paste_route import paste_bp
-    app.register_blueprint(paste_bp)
-
-    return app
--- app/routes/paste_route.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint,render_template
-
-paste_bp = Blueprint('paste_bp', __name__)
-@paste_bp.route('/')
-def paste():
-    return render_template('index.html')
-
